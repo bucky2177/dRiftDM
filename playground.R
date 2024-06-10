44535c81dc1262ba5cbfb9f9d51d9f3a640a0eff
bucky2177 <koob@uni-bremen.de>
2024-06-10 18:12:33 +0200

    tiny edits and im_zero

--- playground.R
+++ playground.R
@@ -5,20 +5,35 @@ library(tictoc)
 load_all()
 rm(list = ls())
 
-a_model = dmc_dm(dt = 0.001, dx = 0.001)
+a_model = dmc_dm(dt = 0.001, dx = 0.001, t_max = 1)
+a_model = set_comp_funs(a_model, comp_funs = list(x_fun = component_shelf()$x_dirac_0))
 
-a_model$prms_model[["alpha"]]=1.5
-a_model$prms_model[["tau"]]=0.01
+a_model = set_solver_settings(a_model, new_solver_vals = c("solver" = "im_zero"))
 
-all_comps = component_shelf()
-#a_model = set_comp_funs(a_model, comp_funs = list(x_fun = all_comps$x_dirac_0))
-a_model = set_comp_funs(a_model, comp_funs = list(nt_fun = all_comps$nt_constant))
-a_model = set_comp_funs(a_model, comp_funs = list(x_fun = all_comps$x_uniform))
-a_model$prms_model[["range_start"]] = 0.1
 tic()
-a_model=re_evaluate_model(a_model)
+a_model = re_evaluate_model(a_model)
 toc()
 
-min(unlist(a_model$pdfs))
-plot(a_model$pdfs$comp$pdf_u+a_model$pdfs$comp$pdf_l,type='l',col='red')
-points(a_model$pdfs$comp$pdf_l,type='l',col='orange')
+pdf_u = a_model$pdfs$comp$pdf_u
+pdf_l = a_model$pdfs$comp$pdf_l
+
+head(pdf_u)
+tail(pdf_u)
+head(pdf_l)
+tail(pdf_l)
+plot(pdf_u, ty = "l")
+points(pdf_l, ty = "l", col = "red")
+
+
+
+a_model = set_solver_settings(a_model, new_solver_vals = c("solver" = "kfe"))
+
+tic()
+a_model = re_evaluate_model(a_model)
+toc()
+
+pdf_u = a_model$pdfs$comp$pdf_u
+pdf_l = a_model$pdfs$comp$pdf_l
+
+plot(pdf_u, ty = "l")
+points(pdf_l, ty = "l", col = "red")
